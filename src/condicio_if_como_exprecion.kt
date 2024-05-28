c86601f357067ef751c231e3549c2b33b39f69a5
Randallmid <Liamboxd@gmail.com>
2024-05-28 10:55:13 -0600

    ahora completo

---
+++ src/condicio_if_como_exprecion.kt
@@ -0,0 +1,42 @@
+fun main () {
+    /* //problema 1
+
+    println("ingrese el primer valor")
+    val val1 = readln().toInt()
+    println("ingrese el segundo valor")
+    val val2 = readln().toInt()
+    val mayor= if (val1>val2) val1 else val2
+
+    println("el mayor entre $val1 y $val2 es $mayor")
+
+
+     */
+/*
+    //problema2
+    println("ingrese un valor entero: ")
+    val valor = readln().toInt()
+
+    val resultado = if (valor % 2 ==0 ){
+    println("cuadrado:")
+        valor*valor}else{
+            println("cubo")
+        valor*valor*valor }
+    println(resultado)
+
+
+ */
+
+    println("ingrese un valor")
+    val valoer = readln().toInt()
+    val digi = if (valoer>9) {
+        println("tu valor tiene 2 digitos")} else {
+            println("tu valor tiene 1 digito ")
+
+        }
+
+
+
+
+
+
+    }

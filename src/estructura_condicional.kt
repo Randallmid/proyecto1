beaabe9ee62ed58fd5cd2662b0503e224518065c
Randallmid <Liamboxd@gmail.com>
2024-05-28 10:09:16 -0600

    mayorca

---
+++ src/estructura_condicional.kt
@@ -0,0 +1,71 @@
+fun main() {
+    /*
+    //problema 1
+    print("ingrese el sueldo del empleado")
+    val sueldo : Double= readln(). toDouble ()
+    //condicional if si el sueldo es mayor a 3000
+    //imprimir Debe pagar impuestos
+    if( sueldo>3000){
+        println ("debe pagar impuetos")
+    }
+    if(sueldo<3000){println ("no pague nada bby")}
+
+     */
+    /*
+//problema 2
+    println("ingrese primer valor")
+    val valor1 = readln().toInt()
+    println("ingrese segundo valor")
+    val valor2 = readln().toInt()
+//condicional if /else
+    if (valor1 > valor2) {
+        print("el mayor valor es el $valor1 ")
+    } else {
+        print("el mayor valor es el $valor2")
+    }
+
+
+}
+
+     */
+    /*
+    //problema 33
+    println("ingrese el primer valoreishon")
+    val valor21= readln().toInt()
+    println("ingrese el segundo valoreishon")
+    val valor22= readln().toInt()
+if (valor21<valor22){
+    val suma: Int =valor21+valor22
+    println("la suma de los valores es $suma")
+
+    val resta: Int =valor21-valor22
+    println("la resta de los valores es $resta" )
+} else {
+        val division : Int =valor21/valor22
+        println("la division de los valores es $division")
+
+        val multiplicacion: Int =valor21*valor22
+        println("la resta de los valores es $multiplicacion" )}
+    */
+    /*
+//problema 4
+    println ("ingrese las notas de el estudiante")
+    val nota1 = readln().toInt()
+    val nota2 = readln().toInt()
+    val nota3 = readln().toInt()
+    val promediouwu= (nota1+nota2+nota3)/3
+    println ("el promedio es $promediouwu")
+    if (promediouwu>=70) {println("promocionado")}
+
+     */
+    //problema 5
+    println("Cuantos digitos tiene tu numero?")
+    val numerouwu = readln().toInt()
+    if (numerouwu >= 10) {
+        println("tu numero tiene 2 digitos")
+    } else {
+        println("tiene solo un digito ")
+    }
+}
+
+
